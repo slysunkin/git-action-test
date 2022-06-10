1f76081e7f25c07b0b4c543426e78762bf16dff8
slysunkin <slysunkin@mirantis.com>
2022-06-10 16:25:16 -0500

    second commit

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, git-action-test!")
-}
+    fmt.Println("Hello, git-action-test!")
 
+    for _, e := range os.Environ() {
+        pair := strings.SplitN(e, "=", 2)
+        fmt.Println(pair[0])
+    }
+}
